4f90e773abed62b2a49d9159124f075072238f3a
ramadhan.fiki2 <ramdhan.fiki2@gmail.com>
2020-05-02 13:08:07 +0700

    add folder dan komponen login

--- SchoolApp/App.js
+++ SchoolApp/App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <LoginScreen />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ SchoolApp/src/screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import FormLogin from '../components/FormLogin'
+
+class Login extends Component {
+  render() {
+    return (
+      <View>
+        <FormLogin />
+      </View>
+    )
+  }
+}
+
+export default Login
