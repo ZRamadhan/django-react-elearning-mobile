8c05b97a0e10e4aad7c802587630ab3d52c26b60
agus randhani <agus.randhani@versafleet.co>
2020-05-16 13:21:06 +0700

    add new package

--- SchoolApp/App.js
+++ SchoolApp/App.js
@@ -1,44 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import EStyleSheet from 'react-native-extended-stylesheet';
-import Navigator from './component/navigator';
-
-const styles = EStyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  confirmButtons: {
-    marginTop: '1rem',
-    flexDirection: 'row',
-  },
-  confirmButtonText: {
-    flex: 1,
-    marginRight: '0.25rem',
-    fontSize: '$small',
-    textAlign: 'right',
-    alignSelf: 'center',
-  },
-  backButtonContainer: {
-    backgroundColor: '$blue',
-    borderWidth: 0,
-  },
-  confirmButtonIcon: {
-    tintColor: '$white',
-  },
-  copyUUID: {
-    color: '$blue',
-  },
-});
+import Navigator from './src/component/navigator';
+import { MenuProvider } from 'react-native-popup-menu';
 
 export default class App extends Component {
   render() {
     return (
-      <Navigator
-        ref={(nav) => {
-          this.navigator = nav;
-        }}
-      />
+      <MenuProvider>
+        <Navigator
+          ref={(nav) => {
+            this.navigator = nav;
+          }}
+        />
+      </MenuProvider>
     )
   }
 }
