f63c48a1583f46867d49136dd6c76a865d4dbfaa
ramadhan.fiki2 <ramdhan.fiki2@gmail.com>
2020-05-30 18:05:33 +0700

    add data to lkn detail

--- SchoolApp/src/pages/lkn/cardPenangkapan.js
+++ SchoolApp/src/pages/lkn/cardPenangkapan.js
@@ -1,40 +1,87 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { connect } from "react-redux";
-import { get_lkn_detail } from "../../reduxActions/dashboard";
-import { get_token } from "../../helper/requestHelper";
 
 class cardPenangkapan extends Component {
-  state = {
-    loading: false,
-  };
-
-  async componentDidMount() {
-    this.setState({ loading: true });
-    //do api call here
-    const token = await get_token();
-    await this.props.dispatch(get_lkn_detail(token, this.props.id));
-    this.setState({ loading: false });
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      data: [],
+    };
   }
 
-  penangkapan = () => {
-    let dataPenangkapan = this.props.lknData.penangkapan
-    dataPenangkapan.map((data, i) => {
-      <Text>
-        
-      </Text>
-    })
+  pushDataToState = async () => {
+    let dataPenangkapan = await this.props.lknData.penangkapan;
+    this.setState({
+      data: dataPenangkapan,
+    });
+  };
+
+  componentDidMount() {
+    this.pushDataToState();
   }
 
   render() {
-    console.log('lkn data', this.props.lknData)
+    console.log("state data", this.state.data);
     return (
       <View>
-        <Text> textInComponent </Text>
+        {this.state.data.map((data, i) => (
+          <TouchableOpacity key={i}>
+            {console.log("hasil map data", data)}
+            <View style={styles.card}>
+              <Text style={{ fontWeight: "bold" }}>
+                {data.no_penangkapan} {"\n"}
+              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <View>
+                  <Text style={{marginBottom: 2}}>Tanggal Penangkapan: </Text>
+                  <View style={styles.separator}/>
+                  <Text>Masa Berakhir Penangkapan: </Text>
+                </View>
+                
+                <View>
+                  <Text style={{marginBottom: 2}}>{data.tanggal_penangkapan}</Text>
+                  <View style={styles.separator}/>
+                  <Text>{data.masa_berakhir_penangkapan}</Text>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    borderColor: "#ddd",
+    borderBottomWidth: 0,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    elevation: 8,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: "#F7F7F7",
+    opacity: 0.85,
+  },
+  separator: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+});
 
 function mapStateToProps(state) {
   const { dashboard } = state;
@@ -43,4 +90,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(cardPenangkapan)
+export default connect(mapStateToProps)(cardPenangkapan);
